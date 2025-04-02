7ebd14db9ce9383d1f798b29bbf39a7b0c38e2dd
Samuel Alvarez <68809623+00Ssam00@users.noreply.github.com>
2025-04-01 21:14:22 -0500

    Archivos Proyecto

---
+++ Reto_2/Reto_2/HomePage.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Reto_2
+{
+    partial class HomePage
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomePage));
+            this.pnl_Barra = new System.Windows.Forms.Panel();
+            this.vsb_Scroll = new System.Windows.Forms.VScrollBar();
+            this.pnl_Principal = new System.Windows.Forms.Panel();
+            this.lbl_Encabezado = new System.Windows.Forms.Label();
+            this.lbl_Parrafo1 = new System.Windows.Forms.Label();
+            this.Llbl_Denuncia = new System.Windows.Forms.LinkLabel();
+            this.btn_Hide = new System.Windows.Forms.Button();
+            this.pb_Encabezado = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn_nocturno = new System.Windows.Forms.Button();
+            this.btn_ayuda = new System.Windows.Forms.Button();
+            this.btn_zoom = new System.Windows.Forms.Button();
+            this.btn_admin = new System.Windows.Forms.Button();
+            this.btn_Inicio = new System.Windows.Forms.Button();
+            this.lbl_Parrafo2 = new System.Windows.Forms.Label();
+            this.pnl_Barra.SuspendLayout();
+            this.pnl_Principal.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Encabezado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnl_Barra
+            // 
+            this.pnl_Barra.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(50)))), ((int)(((byte)(58)))));
+            this.pnl_Barra.Controls.Add(this.button1);
+            this.pnl_Barra.Controls.Add(this.btn_nocturno);
+            this.pnl_Barra.Controls.Add(this.btn_ayuda);
+            this.pnl_Barra.Controls.Add(this.btn_zoom);
+            this.pnl_Barra.Controls.Add(this.btn_admin);
+            this.pnl_Barra.Controls.Add(this.btn_Inicio);
+            this.pnl_Barra.Location = new System.Drawing.Point(3, 138);
+            this.pnl_Barra.Name = "pnl_Barra";
+            this.pnl_Barra.Size = new System.Drawing.Size(1133, 58);
+            this.pnl_Barra.TabIndex = 0;
+            // 
+            // vsb_Scroll
+            // 
+            this.vsb_Scroll.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.vsb_Scroll.Location = new System.Drawing.Point(1130, 1);
+            this.vsb_Scroll.Name = "vsb_Scroll";
+            this.vsb_Scroll.Size = new System.Drawing.Size(16, 630);
+            this.vsb_Scroll.TabIndex = 2;
+            this.vsb_Scroll.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vsb_Scroll_scroll);
+            // 
+            // pnl_Principal
+            // 
+            this.pnl_Principal.BackColor = System.Drawing.Color.White;
+            this.pnl_Principal.Controls.Add(this.lbl_Parrafo2);
+            this.pnl_Principal.Controls.Add(this.btn_Hide);
+            this.pnl_Principal.Controls.Add(this.Llbl_Denuncia);
+            this.pnl_Principal.Controls.Add(this.lbl_Parrafo1);
+            this.pnl_Principal.Controls.Add(this.lbl_Encabezado);
+            this.pnl_Principal.Location = new System.Drawing.Point(225, 202);
+            this.pnl_Principal.Name = "pnl_Principal";
+            this.pnl_Principal.Size = new System.Drawing.Size(726, 512);
+            this.pnl_Principal.TabIndex = 3;
+            // 
+            // lbl_Encabezado
+            // 
+            this.lbl_Encabezado.AutoSize = true;
+            this.lbl_Encabezado.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Encabezado.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(75)))), ((int)(((byte)(82)))));
+            this.lbl_Encabezado.Location = new System.Drawing.Point(60, 21);
+            this.lbl_Encabezado.Name = "lbl_Encabezado";
+            this.lbl_Encabezado.Size = new System.Drawing.Size(596, 39);
+            this.lbl_Encabezado.TabIndex = 0;
+            this.lbl_Encabezado.Text = "Apreciado ciudadano tenga en cuenta";
+            // 
+            // lbl_Parrafo1
+            // 
+            this.lbl_Parrafo1.AutoSize = true;
+            this.lbl_Parrafo1.BackColor = System.Drawing.Color.White;
+            this.lbl_Parrafo1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Parrafo1.Location = new System.Drawing.Point(3, 69);
+            this.lbl_Parrafo1.Name = "lbl_Parrafo1";
+            this.lbl_Parrafo1.Size = new System.Drawing.Size(706, 100);
+            this.lbl_Parrafo1.TabIndex = 1;
+            this.lbl_Parrafo1.Text = resources.GetString("lbl_Parrafo1.Text");
+            // 
+            // Llbl_Denuncia
+            // 
+            this.Llbl_Denuncia.AutoSize = true;
+            this.Llbl_Denuncia.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Llbl_Denuncia.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Llbl_Denuncia.LinkColor = System.Drawing.Color.ForestGreen;
+            this.Llbl_Denuncia.Location = new System.Drawing.Point(3, 218);
+            this.Llbl_Denuncia.Name = "Llbl_Denuncia";
+            this.Llbl_Denuncia.Size = new System.Drawing.Size(161, 37);
+            this.Llbl_Denuncia.TabIndex = 2;
+            this.Llbl_Denuncia.TabStop = true;
+            this.Llbl_Denuncia.Text = "Denuncia:";
+            // 
+            // btn_Hide
+            // 
+            this.btn_Hide.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Hide.Image = global::Reto_2.Properties.Resources.Hide;
+            this.btn_Hide.Location = new System.Drawing.Point(683, 218);
+            this.btn_Hide.Name = "btn_Hide";
+            this.btn_Hide.Size = new System.Drawing.Size(40, 37);
+            this.btn_Hide.TabIndex = 3;
+            this.btn_Hide.UseVisualStyleBackColor = true;
+            this.btn_Hide.Click += new System.EventHandler(this.btn_Hide_Click);
+            // 
+            // pb_Encabezado
+            // 
+            this.pb_Encabezado.Image = global::Reto_2.Properties.Resources.Captura_de_pantalla_2025_04_01_103943;
+            this.pb_Encabezado.Location = new System.Drawing.Point(3, 1);
+            this.pb_Encabezado.Name = "pb_Encabezado";
+            this.pb_Encabezado.Size = new System.Drawing.Size(1133, 141);
+            this.pb_Encabezado.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_Encabezado.TabIndex = 1;
+            this.pb_Encabezado.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Image = global::Reto_2.Properties.Resources.idioma;
+            this.button1.Location = new System.Drawing.Point(963, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(43, 45);
+            this.button1.TabIndex = 5;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btn_nocturno
+            // 
+            this.btn_nocturno.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_nocturno.Image = global::Reto_2.Properties.Resources.nocturno;
+            this.btn_nocturno.Location = new System.Drawing.Point(905, 8);
+            this.btn_nocturno.Name = "btn_nocturno";
+            this.btn_nocturno.Size = new System.Drawing.Size(43, 45);
+            this.btn_nocturno.TabIndex = 4;
+            this.btn_nocturno.UseVisualStyleBackColor = true;
+            // 
+            // btn_ayuda
+            // 
+            this.btn_ayuda.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_ayuda.Image = global::Reto_2.Properties.Resources.ayuda;
+            this.btn_ayuda.Location = new System.Drawing.Point(845, 8);
+            this.btn_ayuda.Name = "btn_ayuda";
+            this.btn_ayuda.Size = new System.Drawing.Size(43, 45);
+            this.btn_ayuda.TabIndex = 3;
+            this.btn_ayuda.UseVisualStyleBackColor = true;
+            // 
+            // btn_zoom
+            // 
+            this.btn_zoom.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_zoom.Image = global::Reto_2.Properties.Resources.zoom;
+            this.btn_zoom.Location = new System.Drawing.Point(786, 8);
+            this.btn_zoom.Name = "btn_zoom";
+            this.btn_zoom.Size = new System.Drawing.Size(43, 45);
+            this.btn_zoom.TabIndex = 2;
+            this.btn_zoom.UseVisualStyleBackColor = true;
+            // 
+            // btn_admin
+            // 
+            this.btn_admin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_admin.Image = global::Reto_2.Properties.Resources.configuracion;
+            this.btn_admin.Location = new System.Drawing.Point(728, 8);
+            this.btn_admin.Name = "btn_admin";
+            this.btn_admin.Size = new System.Drawing.Size(43, 45);
+            this.btn_admin.TabIndex = 1;
+            this.btn_admin.UseVisualStyleBackColor = true;
+            // 
+            // btn_Inicio
+            // 
+            this.btn_Inicio.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(50)))), ((int)(((byte)(58)))));
+            this.btn_Inicio.BackgroundImage = global::Reto_2.Properties.Resources.Captura_de_pantalla_2025_04_01_104725;
+            this.btn_Inicio.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Inicio.ForeColor = System.Drawing.Color.White;
+            this.btn_Inicio.Location = new System.Drawing.Point(179, 10);
+            this.btn_Inicio.Name = "btn_Inicio";
+            this.btn_Inicio.Size = new System.Drawing.Size(39, 41);
+            this.btn_Inicio.TabIndex = 0;
+            this.btn_Inicio.UseVisualStyleBackColor = false;
+            // 
+            // lbl_Parrafo2
+            // 
+            this.lbl_Parrafo2.AutoSize = true;
+            this.lbl_Parrafo2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Parrafo2.Location = new System.Drawing.Point(4, 269);
+            this.lbl_Parrafo2.Name = "lbl_Parrafo2";
+            this.lbl_Parrafo2.Size = new System.Drawing.Size(692, 80);
+            this.lbl_Parrafo2.TabIndex = 4;
+            this.lbl_Parrafo2.Text = resources.GetString("lbl_Parrafo2.Text");
+            // 
+            // HomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.ClientSize = new System.Drawing.Size(1148, 715);
+            this.Controls.Add(this.pnl_Principal);
+            this.Controls.Add(this.vsb_Scroll);
+            this.Controls.Add(this.pb_Encabezado);
+            this.Controls.Add(this.pnl_Barra);
+            this.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "HomePage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PQRSFD ALCALDIA DE LA ESTRELLA";
+            this.pnl_Barra.ResumeLayout(false);
+            this.pnl_Principal.ResumeLayout(false);
+            this.pnl_Principal.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Encabezado)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_Barra;
+        private System.Windows.Forms.PictureBox pb_Encabezado;
+        private System.Windows.Forms.Button btn_Inicio;
+        private System.Windows.Forms.Button btn_admin;
+        private System.Windows.Forms.Button btn_zoom;
+        private System.Windows.Forms.Button btn_ayuda;
+        private System.Windows.Forms.Button btn_nocturno;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.VScrollBar vsb_Scroll;
+        private System.Windows.Forms.Panel pnl_Principal;
+        private System.Windows.Forms.Label lbl_Encabezado;
+        private System.Windows.Forms.Label lbl_Parrafo1;
+        private System.Windows.Forms.Button btn_Hide;
+        private System.Windows.Forms.LinkLabel Llbl_Denuncia;
+        private System.Windows.Forms.Label lbl_Parrafo2;
+    }
+}
+
