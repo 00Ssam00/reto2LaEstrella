303f69028bf238650721f10dd7893400f53a96c5
Kevin Romero Cano <167825805+kvrc2004@users.noreply.github.com>
2025-04-10 15:55:10 -0500

    Form Consultar
    Se inicia el diseño del form consultar pero no se termina por falta de tiempo :(

---
+++ Reto_2/Reto_2/ConsultarPqrs.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace Reto_2
+{
+    partial class ConsultarPqrs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConsultarPqrs));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnl_Barra = new System.Windows.Forms.Panel();
+            this.idiomaButton = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.inicioButton = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.nocturnoButton = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.ayudaButton = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.zoomButton = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.adminButton = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.lbl_Encabezado = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbl_NúmeroRadicado = new System.Windows.Forms.Label();
+            this.gtxt_Radicado = new Guna.UI2.WinForms.Guna2TextBox();
+            this.gbtn_Buscar = new Guna.UI2.WinForms.Guna2Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pnl_Barra.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Reto_2.Properties.Resources.Captura_de_pantalla_2025_04_01_103943;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1404, 201);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pnl_Barra
+            // 
+            this.pnl_Barra.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(50)))), ((int)(((byte)(58)))));
+            this.pnl_Barra.Controls.Add(this.idiomaButton);
+            this.pnl_Barra.Controls.Add(this.inicioButton);
+            this.pnl_Barra.Controls.Add(this.nocturnoButton);
+            this.pnl_Barra.Controls.Add(this.ayudaButton);
+            this.pnl_Barra.Controls.Add(this.zoomButton);
+            this.pnl_Barra.Controls.Add(this.adminButton);
+            this.pnl_Barra.Location = new System.Drawing.Point(-59, 202);
+            this.pnl_Barra.Margin = new System.Windows.Forms.Padding(4);
+            this.pnl_Barra.Name = "pnl_Barra";
+            this.pnl_Barra.Size = new System.Drawing.Size(1524, 67);
+            this.pnl_Barra.TabIndex = 1;
+            // 
+            // idiomaButton
+            // 
+            this.idiomaButton.BackColor = System.Drawing.Color.Transparent;
+            this.idiomaButton.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.idiomaButton.HoverState.ImageSize = new System.Drawing.Size(35, 35);
+            this.idiomaButton.Image = ((System.Drawing.Image)(resources.GetObject("idiomaButton.Image")));
+            this.idiomaButton.ImageOffset = new System.Drawing.Point(0, 0);
+            this.idiomaButton.ImageRotate = 0F;
+            this.idiomaButton.ImageSize = new System.Drawing.Size(35, 35);
+            this.idiomaButton.IndicateFocus = true;
+            this.idiomaButton.Location = new System.Drawing.Point(1289, 17);
+            this.idiomaButton.Margin = new System.Windows.Forms.Padding(4);
+            this.idiomaButton.Name = "idiomaButton";
+            this.idiomaButton.PressedState.ImageSize = new System.Drawing.Size(35, 35);
+            this.idiomaButton.Size = new System.Drawing.Size(47, 43);
+            this.idiomaButton.TabIndex = 11;
+            this.idiomaButton.UseTransparentBackground = true;
+            // 
+            // inicioButton
+            // 
+            this.inicioButton.BackColor = System.Drawing.Color.Transparent;
+            this.inicioButton.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.inicioButton.HoverState.ImageSize = new System.Drawing.Size(35, 35);
+            this.inicioButton.Image = ((System.Drawing.Image)(resources.GetObject("inicioButton.Image")));
+            this.inicioButton.ImageOffset = new System.Drawing.Point(0, 0);
+            this.inicioButton.ImageRotate = 0F;
+            this.inicioButton.ImageSize = new System.Drawing.Size(35, 35);
+            this.inicioButton.IndicateFocus = true;
+            this.inicioButton.Location = new System.Drawing.Point(241, 14);
+            this.inicioButton.Margin = new System.Windows.Forms.Padding(4);
+            this.inicioButton.Name = "inicioButton";
+            this.inicioButton.PressedState.ImageSize = new System.Drawing.Size(35, 35);
+            this.inicioButton.Size = new System.Drawing.Size(47, 43);
+            this.inicioButton.TabIndex = 10;
+            this.inicioButton.UseTransparentBackground = true;
+            // 
+            // nocturnoButton
+            // 
+            this.nocturnoButton.BackColor = System.Drawing.Color.Transparent;
+            this.nocturnoButton.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.nocturnoButton.HoverState.ImageSize = new System.Drawing.Size(35, 35);
+            this.nocturnoButton.Image = ((System.Drawing.Image)(resources.GetObject("nocturnoButton.Image")));
+            this.nocturnoButton.ImageOffset = new System.Drawing.Point(0, 0);
+            this.nocturnoButton.ImageRotate = 0F;
+            this.nocturnoButton.ImageSize = new System.Drawing.Size(35, 35);
+            this.nocturnoButton.IndicateFocus = true;
+            this.nocturnoButton.Location = new System.Drawing.Point(1211, 17);
+            this.nocturnoButton.Margin = new System.Windows.Forms.Padding(4);
+            this.nocturnoButton.Name = "nocturnoButton";
+            this.nocturnoButton.PressedState.ImageSize = new System.Drawing.Size(35, 35);
+            this.nocturnoButton.Size = new System.Drawing.Size(47, 43);
+            this.nocturnoButton.TabIndex = 9;
+            this.nocturnoButton.UseTransparentBackground = true;
+            // 
+            // ayudaButton
+            // 
+            this.ayudaButton.BackColor = System.Drawing.Color.Transparent;
+            this.ayudaButton.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.ayudaButton.HoverState.ImageSize = new System.Drawing.Size(35, 35);
+            this.ayudaButton.Image = ((System.Drawing.Image)(resources.GetObject("ayudaButton.Image")));
+            this.ayudaButton.ImageOffset = new System.Drawing.Point(0, 0);
+            this.ayudaButton.ImageRotate = 0F;
+            this.ayudaButton.ImageSize = new System.Drawing.Size(35, 35);
+            this.ayudaButton.IndicateFocus = true;
+            this.ayudaButton.Location = new System.Drawing.Point(1135, 17);
+            this.ayudaButton.Margin = new System.Windows.Forms.Padding(4);
+            this.ayudaButton.Name = "ayudaButton";
+            this.ayudaButton.PressedState.ImageSize = new System.Drawing.Size(35, 35);
+            this.ayudaButton.Size = new System.Drawing.Size(47, 43);
+            this.ayudaButton.TabIndex = 8;
+            this.ayudaButton.UseTransparentBackground = true;
+            // 
+            // zoomButton
+            // 
+            this.zoomButton.BackColor = System.Drawing.Color.Transparent;
+            this.zoomButton.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.zoomButton.HoverState.ImageSize = new System.Drawing.Size(35, 35);
+            this.zoomButton.Image = ((System.Drawing.Image)(resources.GetObject("zoomButton.Image")));
+            this.zoomButton.ImageOffset = new System.Drawing.Point(0, 0);
+            this.zoomButton.ImageRotate = 0F;
+            this.zoomButton.ImageSize = new System.Drawing.Size(35, 35);
+            this.zoomButton.IndicateFocus = true;
+            this.zoomButton.Location = new System.Drawing.Point(1057, 17);
+            this.zoomButton.Margin = new System.Windows.Forms.Padding(4);
+            this.zoomButton.Name = "zoomButton";
+            this.zoomButton.PressedState.ImageSize = new System.Drawing.Size(35, 35);
+            this.zoomButton.Size = new System.Drawing.Size(47, 43);
+            this.zoomButton.TabIndex = 7;
+            this.zoomButton.UseTransparentBackground = true;
+            // 
+            // adminButton
+            // 
+            this.adminButton.BackColor = System.Drawing.Color.Transparent;
+            this.adminButton.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.adminButton.HoverState.ImageSize = new System.Drawing.Size(35, 35);
+            this.adminButton.Image = ((System.Drawing.Image)(resources.GetObject("adminButton.Image")));
+            this.adminButton.ImageOffset = new System.Drawing.Point(0, 0);
+            this.adminButton.ImageRotate = 0F;
+            this.adminButton.ImageSize = new System.Drawing.Size(35, 35);
+            this.adminButton.IndicateFocus = true;
+            this.adminButton.Location = new System.Drawing.Point(983, 17);
+            this.adminButton.Margin = new System.Windows.Forms.Padding(4);
+            this.adminButton.Name = "adminButton";
+            this.adminButton.PressedState.ImageSize = new System.Drawing.Size(35, 35);
+            this.adminButton.Size = new System.Drawing.Size(47, 43);
+            this.adminButton.TabIndex = 6;
+            this.adminButton.UseTransparentBackground = true;
+            // 
+            // lbl_Encabezado
+            // 
+            this.lbl_Encabezado.AutoSize = true;
+            this.lbl_Encabezado.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Encabezado.Location = new System.Drawing.Point(228, 273);
+            this.lbl_Encabezado.Name = "lbl_Encabezado";
+            this.lbl_Encabezado.Size = new System.Drawing.Size(937, 78);
+            this.lbl_Encabezado.TabIndex = 2;
+            this.lbl_Encabezado.Text = "Ingrese el número de radicado de la solicitud realizada, para \r\nverificar en que " +
+    "estado se encuentra";
+            this.lbl_Encabezado.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbl_NúmeroRadicado);
+            this.panel1.Location = new System.Drawing.Point(300, 427);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(171, 38);
+            this.panel1.TabIndex = 3;
+            // 
+            // lbl_NúmeroRadicado
+            // 
+            this.lbl_NúmeroRadicado.AutoSize = true;
+            this.lbl_NúmeroRadicado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_NúmeroRadicado.Location = new System.Drawing.Point(12, 11);
+            this.lbl_NúmeroRadicado.Name = "lbl_NúmeroRadicado";
+            this.lbl_NúmeroRadicado.Size = new System.Drawing.Size(153, 20);
+            this.lbl_NúmeroRadicado.TabIndex = 0;
+            this.lbl_NúmeroRadicado.Text = "Número Radicado: ";
+            // 
+            // gtxt_Radicado
+            // 
+            this.gtxt_Radicado.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.gtxt_Radicado.DefaultText = "";
+            this.gtxt_Radicado.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.gtxt_Radicado.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.gtxt_Radicado.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.gtxt_Radicado.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.gtxt_Radicado.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.gtxt_Radicado.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gtxt_Radicado.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.gtxt_Radicado.Location = new System.Drawing.Point(472, 427);
+            this.gtxt_Radicado.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.gtxt_Radicado.Name = "gtxt_Radicado";
+            this.gtxt_Radicado.PlaceholderText = "Escriba su número de radicado aquí";
+            this.gtxt_Radicado.SelectedText = "";
+            this.gtxt_Radicado.Size = new System.Drawing.Size(473, 38);
+            this.gtxt_Radicado.TabIndex = 4;
+            // 
+            // gbtn_Buscar
+            // 
+            this.gbtn_Buscar.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.gbtn_Buscar.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.gbtn_Buscar.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.gbtn_Buscar.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.gbtn_Buscar.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.gbtn_Buscar.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gbtn_Buscar.ForeColor = System.Drawing.Color.White;
+            this.gbtn_Buscar.Location = new System.Drawing.Point(942, 427);
+            this.gbtn_Buscar.Name = "gbtn_Buscar";
+            this.gbtn_Buscar.Size = new System.Drawing.Size(155, 38);
+            this.gbtn_Buscar.TabIndex = 5;
+            this.gbtn_Buscar.Text = "Buscar";
+            // 
+            // ConsultarPqrs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1406, 742);
+            this.Controls.Add(this.gbtn_Buscar);
+            this.Controls.Add(this.gtxt_Radicado);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lbl_Encabezado);
+            this.Controls.Add(this.pnl_Barra);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ConsultarPqrs";
+            this.Text = "ConsultarPqrs";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pnl_Barra.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel pnl_Barra;
+        private Guna.UI2.WinForms.Guna2ImageButton idiomaButton;
+        private Guna.UI2.WinForms.Guna2ImageButton inicioButton;
+        private Guna.UI2.WinForms.Guna2ImageButton nocturnoButton;
+        private Guna.UI2.WinForms.Guna2ImageButton ayudaButton;
+        private Guna.UI2.WinForms.Guna2ImageButton zoomButton;
+        private Guna.UI2.WinForms.Guna2ImageButton adminButton;
+        private System.Windows.Forms.Label lbl_Encabezado;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbl_NúmeroRadicado;
+        private Guna.UI2.WinForms.Guna2TextBox gtxt_Radicado;
+        private Guna.UI2.WinForms.Guna2Button gbtn_Buscar;
+    }
+}
